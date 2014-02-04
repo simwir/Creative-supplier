9fa1ca15abbe6aabada93c286ceacb08e2cf43fa
simwir <simwir@hotmail.com>
2014-02-04 18:13:04 +0100

    Added debugging

---
+++ common/simwir/cs/Debug.java
@@ -0,0 +1,33 @@
+package simwir.cs;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatMessageComponent;
+
+public class Debug {
+	private static boolean debug = CreativeSupplier.debug;
+	public static void consoleln(String par1Message){
+		if(debug){
+			System.out.println("[Creative-Supplier]"+"[Debug]"+par1Message);
+		}
+	}
+	public void chatln(String par1Message){
+		if(debug){
+			MinecraftServer.getServer().getConfigurationManager().sendChatMsg(ChatMessageComponent.createFromText(par1Message));
+		}
+	}
+	public void chatln(String par1Message, EntityPlayer par2Player){
+		if(debug){
+			par2Player.addChatMessage("[Creative-Supplier]"+"[Debug]"+par1Message);
+		}
+	}
+	/*
+	public static void chatDebuggerln(String par1Message){
+		EntityPlayer player = CreativeSupplier.debugger;
+		if(debug){
+			
+		}
+	}
+	*/
+
+}
